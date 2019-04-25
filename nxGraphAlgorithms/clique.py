34af4eb5b8c9cc6c96590602ded2b196429f5d58
lexi11095 <49996624+lexi11095@users.noreply.github.com>
2019-04-25 15:06:13 -0500

    Update clique.py

--- nxGraphAlgorithms/clique.py
+++ nxGraphAlgorithms/clique.py
@@ -1,15 +1,12 @@
 import networkx as nx
-import matplotlib.pyplot as plt
 from itertools import combinations
 from functions.bool_functions import *
 
 def maximum_clique_set(G):
-    for i in range (len(S)):
-        N = neighbors(G, S[i])
-        for j in range(i + 1, len(S)):
-            if S[i + 1] not in N:
-                return False
-    return True
+   for k in range (n(G), 1, -1):
+       for S in combinations(V(G), k):
+           if is_clique(G, list(S)) == True:
+               return list(S)
 
 def clique_number(G):
     return len(maximum_clique_set(G))
